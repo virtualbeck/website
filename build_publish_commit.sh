6b7dfaa2eadc80d9deee4854da84715918a272d7
linux <virtualbeck@gmail.com>
2025-01-05 08:47:08 -0600

    Sun Jan  5 08:47:08 AM CST 2025 commit to master

--- build_publish_commit.sh
+++ build_publish_commit.sh
@@ -5,20 +5,21 @@ hugo
 
 if [ $? -eq 0 ]
 then
-  echo "Successfully built site."
-    echo "Proceeding with deployment..."
-
-    # Commit and push changes
-    git add -A
-    commit_ret=$(git commit -m "$(date) commit to master")
-    if [[ $commit_ret == *"nothing to commit"* ]]; then
-      echo "No changes to commit, exiting publish step."
-      exit 0
-    else
-      git push
-      scp -r public/ /home/linux/docker-data/virtualbeck_blog/
-      cd ~/docker-data/traefik && docker-compose restart virtualbeck_nginx
-    fi
+  echo "Successfully built site. Proceeding with deployment..."
+  # Commit and push changes
+  git add -A
+  commit_ret=$(git commit -m "$(date) commit to master")
+  if [[ $commit_ret == *"nothing to commit"* ]]; then
+    echo "No changes to commit, exiting publish step."
+    exit 0
+  else
+    git push
+    # remove old generated stylesheet(s)
+    rm /home/linux/docker-data/virtualbeck_blog/public/assets/css/stylesheet.*.css 
+    
+    cp -a public/ /home/linux/docker-data/virtualbeck_blog/
+    cd ~/docker-data/traefik && docker-compose restart virtualbeck_nginx
+  fi
 else
   echo "Build failed..." >&2
   exit 1
