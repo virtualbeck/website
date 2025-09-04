985462bb2d6060a18157620bdf06b868941e73e3
Mike Beck <mbeck@gofigg.com>
2025-09-03 21:40:00 -0500

    Wed Sep  3 09:40:00 PM CDT 2025 commit to master

--- build_publish_commit.sh
+++ build_publish_commit.sh
@@ -13,16 +13,18 @@ then
   echo "Successfully built site. Proceeding with deployment..."
   # Commit and push changes
   git add -A
+  rm -rf /tmp/public/
   commit_ret=$(git commit -m "$(date) commit to master")
   if [[ $commit_ret == *"nothing to commit"* ]]; then
     echo "No changes to commit, exiting publish step."
     exit 0
   else
     git push
-    cp -a public/ /home/linux/docker-data/virtualbeck_blog/
+    cp -a public/ ../
     cd ~/docker-data/traefik && docker compose restart virtualbeck_nginx
   fi
 else
   echo "Build failed..." >&2
+  mv /tmp/public .
   exit 1
 fi
